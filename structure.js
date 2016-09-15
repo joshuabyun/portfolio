beb659a283db138b55b30b1ff516c91f092a3d28
Joshua Byun <byunsung89@gmail.com>
2016-09-14 18:03:54 -0700

    template properly changing size upon click - basic structure finished

--- structure.js
+++ structure.js
@@ -1,12 +1,42 @@
 $(document).ready(function(){
-   addClickHandler()
+    initClickHandler()
 });
 
+var elementId = ['box1','box2','box3','box4'];
 
-function addClickHandler(){
+function initClickHandler(){
     $('.content').click(function(){
-            $(this).addClass('largerDiv');
-            $('#box2,#box3, #box4').addClass('smallDiv');
+        var clickedDivId = $(this).attr("id");
+        if($(this).attr('largeDivClicked')){//back to default size
+            changeClickedDiv(clickedDivId,"toDefault");
+            changeNonClickedDiv(clickedDivId,"toDefault");
+            $(this).removeAttr("largeDivClicked");
+        }else{
+            changeClickedDiv(clickedDivId);
+            changeNonClickedDiv(clickedDivId);
+            $(this).attr({"largeDivClicked":true});
         }
-    )
+    })
+}
+
+function changeClickedDiv(clickedDivId,state){
+    if(state == "toDefault"){
+        $("#"+clickedDivId).removeClass("largerDiv");
+    }else{
+        $("#"+clickedDivId).addClass("largerDiv");
+    }
+
+}
+
+function changeNonClickedDiv(clickedDivId,state){
+    for(var i = 0; i < elementId.length; i++){
+       if(clickedDivId != elementId[i]){
+           if(state == "toDefault"){
+               $("#"+elementId[i]).removeClass("smallDiv");
+           }
+           else{
+               $("#"+elementId[i]).addClass("smallDiv");
+           }
+       }
+    }
 }
